b4e91ecbdfeb4934f046eca859023366a94fff06
birukdesta <birukb500@gmail.com>
2023-03-22 13:56:36 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,21 @@
 #include "main.h"
+
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0
  */
-int main(void)
+
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
 }
